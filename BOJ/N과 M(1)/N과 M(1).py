584845711813fe945ca1aa9619cb279e60318815
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2022-02-08 23:07:13 +0900

    N과 M(O)
    실버 - 15649

---
+++ BOJ/N과 M(1)/N과 M(1).py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+from itertools import permutations  # 순열 라이브러리 import
+
+N,M = map(int,input().split())  # N과 M 입력받기
+number_list = [i for i in range(1,N+1)] # 1부터 N까지의 number_list 생성
+answer = list(permutations(number_list,M))  # 그 중에서 중복 없이 M개를 고른 순열 생성하고 리스트화
+for x in answer:  # answer리스트에서 x를 그냥 뽑아내면, 튜플형태로 나오므로 추출 필요 
+    print(*x)    # 튜플의 모든 값 추출 
