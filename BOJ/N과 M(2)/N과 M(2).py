01729f5a2a070adc1f08ade14a573d6b2b26820d
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2022-02-15 22:02:19 +0900

    N과 M(O)

--- BOJ/N과 M(2)/N과 M(2).py
+++ BOJ/N과 M(2)/N과 M(2).py
@@ -1,12 +1,9 @@
-## commmit
+import sys
+input = sys.stdin.readline
+from itertools import permutations,combinations
 
-ㅗㅑㅗㅇ
-ㅇ
-ㅇ
-ㅇ
-
-ㅇ
-ㅇ
-ㅇㅇ
-ㅇ
-ㅇㅇㅇㅇㅇ
+n,m = map(int,input().split())  # n과 m을 입력받기
+answer_list = []  # answer_list 라는 빈 리스트 생성
+answer_list = combinations(range(1,n+1),m)  # 1부터 n+1 까지의 수를 m 개의 조합을 모두 answer_list 라 한다.
+for i in answer_list: # answer_list 에 있는 모든 조합을 
+    print(* i)  # 튜플 형태가 아닌 수 형태로 출력한다.
