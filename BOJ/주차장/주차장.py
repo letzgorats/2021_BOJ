f3f6e83b894706c5916c8e61d72b1c8b7df9d410
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2022-03-23 00:29:16 +0900

    주차장_(O)

--- BOJ/주차장/주차장.py
+++ BOJ/주차장/주차장.py
@@ -1,3 +1,7 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
 N, M = map(int,input().split()) # 주차 공간, 차량 수 
 fare = [0]
 for _ in range(N):
@@ -13,14 +17,12 @@ for _ in range(2*M):    # 차량이 들어오고 나가는 출입 리스트
 queue = deque(enter_order)
 # 1번 자리 부터 N번자리 까지 주차가능한지의 여부와 차 번호를 나타내는 리스트
 possible_lot = [[True,0] for _ in range(N+1)] 
-# [[0], [0], [0], [0]]
-print(possible_lot)
+
 answer = 0
+wait_for = deque([]) # 주차 공간이 현재 꽉차있을 때 대기하는 차 리스트
 while queue:
-    car = queue.popleft()
-    wait_for = deque([]) # 주차 공간이 현재 꽉차있을 때 대기하는 차 리스트
+    car = queue.popleft()   
     wait_queue = True # 대기하는 차가 발생하는지 나타내는 변수
-    print("car= ", car)
     if car > 0 :    # 차가 들어오는 경우
         for i in range(1,N+1):
             if possible_lot[i][0] == True : # 비어있으면 그 자리 주차
@@ -29,10 +31,8 @@ while queue:
                 possible_lot[i][0] = False # 해당 자리(i)는 주차 불가를 뜻하는 False로 변경                
                 wait_queue = False
                 break   
-        if wait_queue == True:  # 대기하는 차가 발생했다면
-            
+        if wait_queue:  # 대기하는 차가 발생했다면
             wait_for.append(car) # 대기열에 그 차를 넣어준다.
-        print(wait_for)
     else:   # 차가 나가는 경우
         car = -car # 음수의 car를 양수로 바꾸고
         for i in range(1,N+1):
@@ -43,6 +43,5 @@ while queue:
                     possible_lot[i][0] = False # 해당 주차자리 채우고
                     possible_lot[i][1] = car # 해당 주차자리는 그 차가 차지
                     answer += weight[car] * fare[i] # 주차료 계산
-                    break
                 break
 print(answer)
