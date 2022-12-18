cf9afa2cd42fae7d7a71fd0a8f437afae97b0e5c
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2022-12-18 09:49:52 +0900

    구현

--- Programmers/level2/124 나라의 숫자/124 나라의 숫자.py
+++ Programmers/level2/124 나라의 숫자/124 나라의 숫자.py
@@ -1,34 +1,18 @@
-import itertools
-
 def solution(n):
     answer = ''
-    sets = "124"
-    
+    while n:
+        if n % 3 != 0:  # 3의 배수가 아니라면,
+            answer += str(n % 3)
+            n //= 3 
+        else:   # 3의 배수라면,
+            answer += "4"
+            n = n//3 - 1
+    return answer[::-1]
+
+def solution(n):
     if n <= 3:
-        if n == 1: answer = 1
-        elif n == 2 : answer = 2
-        else: answer = 4
-    
+        return '124'[n-1]
     elif n > 3:
-        exponent = 0
-        tmp = 0
-        for i in range(1,n):
-            tmp += 3 ** i
-            if tmp > n :
-                tmp -= 3** i
-                exponent = i
-                break
-        
-        tmp_share = n // tmp 
-        tmp_mod = n % tmp
-        data = itertools.product(sets, repeat = exponent)
-    
-        for idx,x in enumerate(data):
-            if idx == tmp_mod -1:
-                answer = ''.join(x)
-                break
-                # print(x,answer)
-        # answer = data[tmp_mod-1]
-        # print(tmp_share, tmp_mod)
-        # print(answer)
-    return str(answer)
+        q,r = divmod(n-1,3)
+        return solution(q) + '124'[r]
+
