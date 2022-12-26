c06fb68ef925ba3b26f6b9f6bfd67f60327593c7
LetzGoRats <77396189+letzgorats@users.noreply.github.com>
2022-12-26 15:41:46 +0900

    DFS/BFS

--- Programmers/level2/타겟 넘버/타겟 넘버.py
+++ Programmers/level2/타겟 넘버/타겟 넘버.py
@@ -16,3 +16,25 @@ def solution(numbers, target):
             if temp == target :
                 answer += 1
     return answer
+
+
+
+# DFS 로 푸는 방법
+def solution(numbers, target):
+    
+    idx = 0 
+    answer = 0
+    n = len(numbers)
+    def dfs(idx,results):
+         
+        if idx < n:
+            dfs(idx+1,results + numbers[idx])
+            dfs(idx+1,results - numbers[idx])
+        else:
+            nonlocal answer
+            if target == results:
+                answer += 1
+            return 
+    dfs(0,0)
+    
+    return answer
